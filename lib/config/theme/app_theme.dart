2de2236cdabf8670223ae12bf663f54887b65ee9
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-06-15 11:12:36 -0600

    Adding global theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.teal,
+  Colors.orange,
+  Colors.purpleAccent,
+  Colors.deepPurple
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  const AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'Selected color must be greater then 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less then ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+}
