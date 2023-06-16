adfa289cb4ed700e1bb6af2bce57caa2b9d14bd4
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-06-16 15:32:19 -0600

    Progress indicator finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -26,5 +26,11 @@ const appMenuItems = <MenuItem>[
     subTitle: 'Un contenido con estilo',
     link: '/cards',
     icon: Icons.credit_card,
+  ),
+  MenuItem(
+    title: 'Progress Indicator',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
   )
 ];
